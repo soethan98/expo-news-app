1ec341621a9b8ae4b46b9da064c1a47471581487
soethan98 <soethan762@gmail.com>
2025-05-14 21:53:48 +0800

    useState hook to persist selectedCategory in categorries list

--- screens/Home/components/Categories.js
+++ screens/Home/components/Categories.js
@@ -1,16 +1,25 @@
-import { StyleSheet,View,Text, ScrollView, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Text, ScrollView, TouchableOpacity } from "react-native";
 import newCategoryList from "../../../constants/categories";
 import { GlobalStyles } from "../../../constants/colors";
+import { useState } from "react";
 const Categories = () => {
+    const [selectedCategory, setSelectedCategory] = useState(null)
     return (<View>
         <Text style={styles.title}>Trending Right Now</Text>
-       <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.itemsWrapper}>
-        {newCategoryList.map((category)=> (
-            <TouchableOpacity key={category.id} style={styles.chip}>
-                <Text style={styles.chipText}>{category.title}</Text>
-            </TouchableOpacity>
-        ))}
-       </ScrollView>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.itemsWrapper}>
+            {newCategoryList.map((category) => (
+                <TouchableOpacity key={category.id} style={[
+                    styles.chip , selectedCategory === category.id && styles.chipSelected,
+                ]} onPress={() => {
+                    setSelectedCategory(category.id)
+                }}>
+                    <Text style={[
+                        styles.chipText,
+                        selectedCategory === category.id && styles.chipTextSelected
+                    ]}>{category.title}</Text>
+                </TouchableOpacity>
+            ))}
+        </ScrollView>
     </View>);
 }
 
@@ -19,27 +28,35 @@ export default Categories;
 
 
 const styles = StyleSheet.create({
-    title:{
-        fontSize:18,
-        marginHorizontal:16,
-        marginVertical:24,
-        fontWeight:'600',
+    title: {
+        fontSize: 18,
+        marginHorizontal: 16,
+        marginVertical: 24,
+        fontWeight: '600',
     },
-    itemsWrapper:{
-        paddingVertical:10,
-        paddingHorizontal:20,
-        gap:8
-
-    
+    itemsWrapper: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        gap: 8
     },
-    chip:{
-        borderWidth:1,
-        borderColor:GlobalStyles.colors.darkGrey,
+    chip: {
+        borderWidth: 1,
         paddingHorizontal: 16,
         paddingVertical: 8,
         borderRadius: 12,
     },
-    chipSelected:{},
-    chipText:{},
-    chipTextSelected:{}
+    chipSelected: {
+
+        backgroundColor:GlobalStyles.colors.tabIconSelected,
+        borderColor:GlobalStyles.colors.tabIconSelected,
+    },
+    chipText: {
+        fontSize: 14,
+        letterSpacing: 0.5,
+        color: GlobalStyles.colors.darkGrey
+    },
+    chipTextSelected: {
+        color:GlobalStyles.colors.white,
+        
+    }
 })
