8d5a0ba6ff642d10efbf687a45eb83babd82f2f8
soethan98 <soethan762@gmail.com>
2025-05-31 17:07:15 +0800

    Added asynstorage to cache favoriteIds

--- store/FavoritesContext.js
+++ store/FavoritesContext.js
@@ -1,4 +1,5 @@
-import { createContext,useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createContext,use,useEffect,useState } from "react";
 
 
 
@@ -11,6 +12,34 @@ export const FavoritesContext = createContext({
 
 export default function FavoritesContextProvider({ children }) {
     const [favorites, setFavorites] = useState([]);
+
+    // Load favorites from AsyncStorage on initial render
+    useEffect(() => {
+        const loadFavorites = async () => {
+            try {
+                const storedFavorites = await AsyncStorage.getItem('favorites');
+                if(storedFavorites !== null){
+                    setFavorites(JSON.parse(storedFavorites));
+                }
+            } catch (error) {
+                console.error("Failed to load favorites:", error);
+                
+            }
+        }
+        loadFavorites();
+    },[]);
+
+
+    useEffect(() => {
+        const saveFavorites = async () => {
+            try {
+                await AsyncStorage.setItem('favorites', JSON.stringify(favorites));
+            } catch (error) {
+                console.error("Failed to save favorites:", error);
+            }
+        }
+        saveFavorites();
+    },[favorites]);
 
     function addFavorite(id) {
         setFavorites((currentFavorites) => [...currentFavorites, id]);
