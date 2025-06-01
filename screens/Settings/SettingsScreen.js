03fb6137d0cc324d300ce13c8be5f09fdb795690
soethan98 <soethan762@gmail.com>
2025-06-01 14:55:11 +0800

    Added Settings screen UI

--- screens/Settings/SettingsScreen.js
+++ screens/Settings/SettingsScreen.js
@@ -1,11 +1,51 @@
 
 
 
-import { StyleSheet, View,Text } from "react-native";
+import { StyleSheet, View,Text,Switch, TouchableOpacity } from "react-native";
+import { GlobalStyles } from "../../constants/colors";
+import {MaterialIcons} from '@expo/vector-icons';
+import { useState } from "react";
 
 function SettingsScreen() {
+    const [isEnabled,setIsEnabled] = useState(false);
+    const toggleSwitch = () => setIsEnabled((prevState) => !prevState);
    return  <View style={styles.container} >
-        <Text>Saved Screen</Text>
+        <TouchableOpacity style={styles.itemBtn}>
+            <Text style={styles.itemBtnText}>
+                About
+            </Text>
+            <MaterialIcons name="arrow-forward-ios" size={16} color={GlobalStyles.colors.black} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.itemBtn}>
+            <Text style={styles.itemBtnText}>
+                Send Feedback
+            </Text>
+            <MaterialIcons name="arrow-forward-ios" size={16} color={GlobalStyles.colors.black} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.itemBtn}>
+            <Text style={styles.itemBtnText}>
+                Privacy Policy
+            </Text>
+            <MaterialIcons name="arrow-forward-ios" size={16} color={GlobalStyles.colors.black} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.itemBtn}>
+            <Text style={styles.itemBtnText}>
+                Terms Of Use
+            </Text>
+            <MaterialIcons name="arrow-forward-ios" size={16} color={GlobalStyles.colors.black} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.itemBtn}>
+            <Text style={styles.itemBtnText}>
+                Dark Mode
+            </Text>
+            <Switch onValueChange={toggleSwitch} value={isEnabled} trackColor={{true : GlobalStyles.colors.tint}}/>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.itemBtn}>
+            <Text style={[styles.itemBtnText,{color:'red'}]}>
+                Logout
+            </Text>
+            <MaterialIcons name="logout" size={16} color='red' />
+        </TouchableOpacity>
     </View>
 }
 
@@ -15,7 +55,18 @@ export default SettingsScreen;
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        justifyContent: "center",
-        alignItems: "center",
-    }
+       padding: 20
+    },
+    itemBtn:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal:16,
+        paddingVertical:20,
+        backgroundColor: GlobalStyles.colors.white
+    },
+    itemBtnText: {
+        fontSize:14,
+        fontWeight:'500',
+        color: GlobalStyles.colors.black
+    },
 })
